27aaf0f150e27e92ab3a292162063eaf6cec749e
Vittorio Romeo <vittorio.romeo@outlook.com>
2015-11-01 17:42:58 +0100

    Added hana-like `is_valid`

---
+++ include/vrm/core/is_valid/is_valid.hpp
@@ -0,0 +1,53 @@
+// Copyright (c) 2015-2016 Vittorio Romeo
+// License: Academic Free License ("AFL") v. 3.0
+// AFL License page: http://opensource.org/licenses/AFL-3.0
+// http://vittorioromeo.info | vittorio.romeo@outlook.com
+
+#pragma once
+
+#include <type_traits>
+#include <vrm/core/config/names.hpp>
+
+VRM_CORE_NAMESPACE
+{
+    namespace impl
+    {
+        struct is_valid_dummy
+        {
+        };
+
+        template <typename T>
+        class is_valid_wrapper
+        {
+        private:
+            template <typename... Ts>
+            VRM_CORE_ALWAYS_INLINE constexpr auto test(is_valid_dummy) noexcept
+                -> decltype(
+                    std::declval<T>()(std::declval<Ts>()...), std::true_type{})
+            {
+                return std::true_type{};
+            }
+
+            template <typename...>
+            VRM_CORE_ALWAYS_INLINE constexpr auto test(...) noexcept
+            {
+                return std::false_type{};
+            }
+
+        public:
+            template <typename... Ts>
+            VRM_CORE_ALWAYS_INLINE constexpr auto operator()(
+                Ts&&... xs) noexcept
+            {
+                return test<decltype(xs)...>(is_valid_dummy{});
+            }
+        };
+    }
+
+    template <typename T>
+    VRM_CORE_ALWAYS_INLINE constexpr auto is_valid(T && )
+    {
+        return impl::is_valid_wrapper<T>{};
+    }
+}
+VRM_CORE_NAMESPACE_END
---
+++ test/core/is_valid/is_valid.cpp
@@ -0,0 +1,113 @@
+#include "../../utils/test_utils.hpp"
+#include <cstddef>
+#include <vrm/core/config.hpp>
+#include <vrm/core/is_valid.hpp>
+#include <vrm/core/for_args.hpp>
+#include <vrm/core/static_if.hpp>
+
+using namespace vrm::core;
+
+struct banana
+{
+    int x;
+    void peel() {}
+    void eat_banana() {}
+};
+
+struct apple
+{
+    int y;
+    void peel() {}
+    void eat_apple() {}
+};
+
+struct chestnut
+{
+    int z;
+    void peel() {}
+};
+
+auto has_peel(is_valid([](auto&& x) -> decltype(x.peel())
+    {
+    }));
+
+auto has_eat_banana(is_valid([](auto&& x) -> decltype(x.eat_banana())
+    {
+    }));
+
+auto has_eat_apple(is_valid([](auto&& x) -> decltype(x.eat_apple())
+    {
+    }));
+
+auto has_x(is_valid([](auto&& x) -> decltype(x.x)
+    {
+    }));
+
+auto has_y(is_valid([](auto&& x) -> decltype(x.y)
+    {
+    }));
+
+auto has_z(is_valid([](auto&& x) -> decltype(x.z)
+    {
+    }));
+
+
+template <typename T>
+struct type_
+{
+    using type = T;
+};
+
+int main()
+{
+    static_assert(has_peel(banana{}), "");
+    static_assert(has_eat_banana(banana{}), "");
+    static_assert(!has_eat_apple(banana{}), "");
+
+    static_assert(has_peel(apple{}), "");
+    static_assert(!has_eat_banana(apple{}), "");
+    static_assert(has_eat_apple(apple{}), "");
+
+    static_assert(has_peel(chestnut{}), "");
+    static_assert(!has_eat_banana(chestnut{}), "");
+    static_assert(!has_eat_apple(chestnut{}), "");
+
+    static_assert(has_x(banana{}), "");
+    static_assert(!has_y(banana{}), "");
+    static_assert(!has_z(banana{}), "");
+
+    static_assert(!has_x(apple{}), "");
+    static_assert(has_y(apple{}), "");
+    static_assert(!has_z(apple{}), "");
+
+    static_assert(!has_x(chestnut{}), "");
+    static_assert(!has_y(chestnut{}), "");
+    static_assert(has_z(chestnut{}), "");
+
+    for_args(
+        [](auto t)
+        {
+            using real_t = typename decltype(t)::type;
+
+            static_if(bool_<has_peel(real_t{})>{})
+                .then([](auto&& x)
+                    {
+                        x.peel();
+                    })(real_t{});
+
+            static_if(bool_<has_eat_banana(real_t{})>{})
+                .then([](auto&& x)
+                    {
+                        x.eat_banana();
+                    })(real_t{});
+
+            static_if(bool_<has_eat_apple(real_t{})>{})
+                .then([](auto&& x)
+                    {
+                        x.eat_apple();
+                    })(real_t{});
+        },
+        type_<banana>{}, type_<apple>{}, type_<chestnut>{});
+
+    return 0;
+}
