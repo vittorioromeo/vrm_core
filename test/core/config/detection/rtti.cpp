8c3639ba2ad3881f3b6c47145272301268b21b93
Vittorio Romeo <vittorio.romeo@outlook.com>
2015-12-04 08:43:19 +0100

    Added RTTI detection

---
+++ test/core/config/detection/rtti.cpp
@@ -0,0 +1,17 @@
+#include "../../../utils/test_utils.hpp"
+#include <vrm/core/config/detection.hpp>
+
+int main()
+{
+    using namespace vrm::core;
+
+    auto current_rtti(detection::rtti());
+
+    TEST_ASSERT(current_rtti == true || current_rtti == false);
+
+#if defined(VRM_CORE_RTTI_ENABLED)
+    (void)typeid(int);
+#endif
+
+    return 0;
+}
