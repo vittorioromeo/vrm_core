9e58fa4c88336942e06369a0d0ec48e54c083a49
Vittorio Romeo <vittorio.romeo@outlook.com>
2015-12-04 18:47:30 +0100

    Added additional for args test

--- test/core/for_args/static_for_args.cpp
+++ test/core/for_args/static_for_args.cpp
@@ -1,9 +1,40 @@
 #include "../../utils/test_utils.hpp"
 #include <vrm/core/for_args/for_args_complete.hpp>
 
+using namespace vrm::core;
+
+void test_continue()
+{
+    sz_t acc = 0;
+
+    static_for_args<1>([&](auto, auto x_wrapper)
+        {
+            constexpr auto x(decltype(x_wrapper){});
+            return static_if(bool_v<(x >= 10)>)
+                .then([&](auto y)
+                    {
+                        return static_if(bool_v<(y == 99)>)
+                            .then([](auto)
+                                {
+                                    return break_t{};
+                                })
+                            .else_([](auto)
+                                {
+                                    return continue_t{};
+                                })(y);
+                    })
+                .else_([&](auto y)
+                    {
+                        acc += y;
+                        return y;
+                    })(x);
+        })(sz_t_v<1>, sz_t_v<10>, sz_t_v<1>, sz_t_v<10>, sz_t_v<99>, sz_t_v<1>);
+
+    TEST_ASSERT_OP(acc, ==, 2);
+}
+
 int main()
 {
-    using namespace vrm::core;
 
 
 
@@ -12,7 +43,7 @@ int main()
             constexpr auto x(decltype(x_wrapper){});
             std::cout << x << "\n";
 
-            return static_if(bool_<(x == 2)>{})
+            return static_if(bool_v<(x == 2)>)
                 .then([](auto y)
                     {
                         return break_t{};
@@ -30,8 +61,8 @@ int main()
     static_for_args([](auto data, auto&& x)
         {
             return static_if(
-                       bool_<std::is_same<std::remove_reference_t<decltype(x)>,
-                           float>{}>{})
+                       bool_v<std::is_same<std::remove_reference_t<decltype(x)>,
+                           float>{}>)
                 .then([](auto&& v)
                     {
                         std::cout << "skip " << decltype(data)::iteration
@@ -52,8 +83,8 @@ int main()
     static_for_args<2>([](auto data, auto&& x, auto&&)
         {
             return static_if(
-                       bool_<std::is_same<std::remove_reference_t<decltype(x)>,
-                           float>{}>{})
+                       bool_v<std::is_same<std::remove_reference_t<decltype(x)>,
+                           float>{}>)
                 .then([](auto&& v)
                     {
                         std::cout << "skip " << decltype(data)::iteration
@@ -68,4 +99,7 @@ int main()
                     })(x);
 
         })(1, "hello", 'a', 3, 4.f, 2, 1, 'b');
+
+
+    test_continue();
 }
