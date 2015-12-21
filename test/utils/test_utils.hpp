eec021bd19cfa145b76a56d9a4a88ccb7be0e403
Vittorio Romeo <vittorio.romeo@outlook.com>
2015-12-21 16:09:48 +0100

    Improve test utils

--- test/utils/test_utils.hpp
+++ test/utils/test_utils.hpp
@@ -5,108 +5,162 @@
 #include <sstream>
 #include <iostream>
 
-#define DEPARENS(...) __VA_ARGS__
+#define TEST_IMPL_DEPARENS(...) __VA_ARGS__
 
 #define SA_SAME(value, type) \
-    static_assert(std::is_same<DEPARENS value, DEPARENS type>{}, "")
+    static_assert(           \
+        std::is_same<TEST_IMPL_DEPARENS value, TEST_IMPL_DEPARENS type>{}, "")
 
 #define SA_TYPE(value, type) \
-    static_assert(std::is_same<decltype value, DEPARENS type>{}, "")
+    static_assert(std::is_same<decltype value, TEST_IMPL_DEPARENS type>{}, "")
+
+#define VRM_CORE_LIKELY(...) __builtin_expect(!!(__VA_ARGS__), 1)
 
 namespace test_impl
 {
-    template <typename T>
-    inline void test_assert(bool x, const T& res, const char* expr) noexcept
+    namespace impl
     {
-        using namespace std::literals;
+        inline auto& get_ostringstream() noexcept
+        {
+            static std::ostringstream oss;
+            return oss;
+        }
 
-        if(x) return;
-        std::ostringstream error;
+        inline auto& clear_and_get_ostringstream() noexcept
+        {
+            auto& oss(get_ostringstream());
+            oss.str("");
+            return oss;
+        }
 
-        error << expr << "\n"
-              << "result: " << res << "\n";
+        [[noreturn]] inline void fail() noexcept
+        {
+            std::cout << get_ostringstream().str() << std::endl;
+            std::terminate();
+        }
 
-        std::cout << error.str() << std::endl;
-        std::terminate();
+        template <typename TStream>
+        inline void output_line(TStream& s, int line)
+        {
+            s << "line: " << line << "\n";
+        }
+
+        template <typename TStream>
+        inline void output_expr(TStream& s, const char* expr)
+        {
+            s << "expr: " << expr << "\n";
+        }
+
+        template <typename TStream, typename T>
+        inline void output_result(TStream& s, const T& lhs_result)
+        {
+            s << "result: " << lhs_result << "\n";
+        }
+
+        template <typename TStream>
+        inline void output_expected(TStream& s, const char* expected)
+        {
+            s << "expected: " << expected << "\n";
+        }
+
+        template <typename TStream, typename T>
+        inline void output_expected(
+            TStream& s, const char* expected, const T& rhs_result)
+        {
+            s << "expected: " << expected << " -> " << rhs_result << "\n";
+        }
+
+        template <typename TF>
+        inline auto do_test(bool x, TF&& f)
+        {
+            if(VRM_CORE_LIKELY(x)) return;
+
+            auto& error(impl::clear_and_get_ostringstream());
+            f(error);
+            impl::fail();
+        }
     }
 
-    template <typename TL, typename TR>
-    inline void test_assert_expected(int line, bool x, TL&& res,
-        TR&& res_r, const char* expr, const char* expected)
+    template <typename T>
+    inline auto test_expr(
+        int line, bool x, T&& lhs_result, const char* expr) noexcept
     {
-        using namespace std::literals;
-
-        if(x) return;
-        std::ostringstream error;
-
-        error << "line: " << line << "\n" << expr << "\n"
-              << "result: " << res << "\n"
-              << "expected: " << expected << " -> " << res_r << "\n";
-
-        std::cout << error.str() << std::endl;
-        std::terminate();
+        return impl::do_test(x, [&](auto& s)
+            {
+                impl::output_line(s, line);
+                impl::output_expr(s, expr);
+                impl::output_result(s, lhs_result);
+            });
     }
 
-    template <typename T>
-    inline void test_assert_ns(bool x, const T&, const char* expr) noexcept
+    template <typename TLhs, typename TRhs>
+    inline auto test_op(int line, bool x, TLhs&& lhs_result, TRhs&& rhs_result,
+        const char* expr, const char* expected)
     {
-        using namespace std::literals;
-
-        if(x) return;
-        std::ostringstream error;
-
-        error << expr << "\n";
-
-        std::cout << error.str() << std::endl;
-        std::terminate();
+        return impl::do_test(x, [&](auto& s)
+            {
+                impl::output_line(s, line);
+                impl::output_expr(s, expr);
+                impl::output_result(s, lhs_result);
+                impl::output_expected(s, expected, rhs_result);
+            });
     }
 
-    template <typename T>
-    inline void test_assert_ns_expected(
-        bool x, const T&, const char* expr, const char* expected)
+    inline auto test_expr_ns(int line, bool x, const char* expr) noexcept
     {
-        using namespace std::literals;
+        return impl::do_test(x, [&](auto& s)
+            {
+                impl::output_line(s, line);
+                impl::output_expr(s, expr);
+            });
+    }
 
-        if(x) return;
-        std::ostringstream error;
-
-        error << expr << "\n"
-              << "expected: " << expected << "\n";
-
-        std::cout << error.str() << std::endl;
-        std::terminate();
+    inline auto test_op_ns(
+        int line, bool x, const char* expr, const char* expected)
+    {
+        return impl::do_test(x, [&](auto& s)
+            {
+                impl::output_line(s, line);
+                impl::output_expr(s, expr);
+                impl::output_expected(s, expected);
+            });
     }
 }
 
-#define TEST_ASSERT(expr)                          \
-    do                                             \
-    {                                              \
-        auto _t_x = expr;                          \
-        test_impl::test_assert(_t_x, expr, #expr); \
+#define TEST_ASSERT(expr)                                  \
+    do                                                     \
+    {                                                      \
+        auto&& _t_x(expr);                                 \
+                                                           \
+        test_impl::test_expr(__LINE__, _t_x, expr, #expr); \
     } while(false)
 
-#define TEST_ASSERT_OP(lhs, op, rhs)                                      \
-    do                                                                    \
-    {                                                                     \
-        auto&& _t_xl = lhs;                                               \
-        auto&& _t_xr = rhs;                                               \
-        using ct = std::common_type_t<decltype(lhs), decltype(rhs)>;      \
-        auto _t_x = ct(_t_xl) op ct(_t_xr);                               \
-        test_impl::test_assert_expected(                                  \
-            __LINE__, _t_x, FWD(_t_xl), FWD(_t_xr), #lhs #op #rhs, #rhs); \
+#define TEST_ASSERT_OP(lhs, op, rhs)                                           \
+    do                                                                         \
+    {                                                                          \
+        using ct = std::common_type_t<decltype(lhs), decltype(rhs)>;           \
+                                                                               \
+        auto&& _t_xl(lhs);                                                     \
+        auto&& _t_xr(rhs);                                                     \
+                                                                               \
+        auto&& _t_x(ct(_t_xl) op ct(_t_xr));                                   \
+                                                                               \
+        test_impl::test_op(__LINE__, _t_x, _t_xl, _t_xr, #lhs #op #rhs, #rhs); \
     } while(false)
 
-#define TEST_ASSERT_NS(expr)                          \
-    do                                                \
-    {                                                 \
-        auto _t_x = expr;                             \
-        test_impl::test_assert_ns(_t_x, expr, #expr); \
+#define TEST_ASSERT_NS(expr)                            \
+    do                                                  \
+    {                                                   \
+        auto&& _t_x(expr);                              \
+                                                        \
+        test_impl::test_expr_ns(__LINE__, _t_x, #expr); \
     } while(false)
 
-#define TEST_ASSERT_NS_OP(lhs, op, rhs)                                       \
-    do                                                                        \
-    {                                                                         \
-        auto _t_xl = lhs;                                                     \
-        auto _t_x = _t_xl op rhs;                                             \
-        test_impl::test_assert_ns_expected(_t_x, _t_xl, #lhs #op #rhs, #rhs); \
+#define TEST_ASSERT_NS_OP(lhs, op, rhs)                             \
+    do                                                              \
+    {                                                               \
+        auto&& _t_xl(lhs);                                          \
+        auto&& _t_x(_t_xl op rhs);                                  \
+                                                                    \
+        test_impl::test_op_ns(__LINE__, _t_x, #lhs #op #rhs, #rhs); \
     } while(false)
