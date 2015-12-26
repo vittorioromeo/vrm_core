d04e6e415e623d347c1bf1f44e4ac0f3e91d094e
Vittorio Romeo <vittorio.romeo@outlook.com>
2015-12-26 22:47:32 +0100

    handles

--- include/vrm/core/experimental/delegate/dynamic_delegate.hpp
+++ include/vrm/core/experimental/delegate/dynamic_delegate.hpp
@@ -14,20 +14,166 @@
 // TODO: WIP:
 // * use sparse int set?
 
-VRM_CORE_NAMESPACE {}
-VRM_CORE_NAMESPACE_END
-
 VRM_CORE_NAMESPACE
 {
     namespace impl
     {
+        template <typename TIndex, typename TCounter>
+        class handle_helper
+        {
+        public:
+            using index_type = TIndex;
+            using counter_type = TCounter;
+
+        private:
+            class handle_data
+            {
+            private:
+                index_type _index;
+                counter_type _counter;
+
+            public:
+                VRM_CORE_ALWAYS_INLINE handle_data(index_type index,
+                    counter_type counter) noexcept : _index{index},
+                                                     _counter{counter}
+                {
+                }
+            };
+
+            struct metadata_impl : handle_data
+            {
+                using handle_data::handle_data;
+
+                VRM_CORE_ALWAYS_INLINE const auto& idx() const noexcept
+                {
+                    return this->_index;
+                }
+
+
+                VRM_CORE_ALWAYS_INLINE const auto& counter() const noexcept
+                {
+                    return this->_counter;
+                }
+            };
+
+            struct handle_impl : handle_data
+            {
+                using handle_data::handle_data;
+
+                VRM_CORE_ALWAYS_INLINE const auto& target_idx() const noexcept
+                {
+                    return this->_index;
+                }
+
+                VRM_CORE_ALWAYS_INLINE const auto& counter() const noexcept
+                {
+                    return this->_counter;
+                }
+            };
+
+        public:
+            using handle_type = handle_impl;
+            using metadata_type = metadata_impl;
+        };
+
+        template <typename THandleHelper>
+        class handle_vector
+        {
+        private:
+            using handle_helper_type = THandleHelper;
+
+        public:
+            using index_type = typename handle_helper_type::index_type;
+            using counter_type = typename handle_helper_type::counter_type;
+            using handle_type = typename handle_helper_type::handle_type;
+            using metadata_type = typename handle_helper_type::metadata_type;
+
+        private:
+            std::vector<metadata_type> _metadata;
+            sz_t _next_idx{0};
+
+            // Get metadata from existing handle.
+            template <typename TSelf>
+            VRM_CORE_ALWAYS_INLINE static decltype(auto)
+            metadata_from_handle_impl(
+                TSelf&& self, const handle_type& h) noexcept
+            {
+                VRM_CORE_ASSERT_OP(self.metadata.size(), >, h.target_idx());
+                return self._metadata[h.target_idx()];
+            }
+
+        public:
+#define VRM_CORE_IMPL_DEFINE_METADATA_FROM_HANDLE(qualifier)    \
+    VRM_CORE_ALWAYS_INLINE decltype(auto) metadata_from_handle( \
+        const handle_type& h) qualifier noexcept                \
+    {                                                           \
+        return metadata_from_handle_impl(*this, h);             \
+    }
+
+            VRM_CORE_IMPL_DEFINE_METADATA_FROM_HANDLE(&)
+            VRM_CORE_IMPL_DEFINE_METADATA_FROM_HANDLE(const&)
+            VRM_CORE_IMPL_DEFINE_METADATA_FROM_HANDLE(&&)
+
+#undef VRM_CORE_IMPL_DEFINE_METADATA_FROM_HANDLE
+
+            VRM_CORE_ALWAYS_INLINE auto valid_handle(const handle_type& h) const
+                noexcept
+            {
+                return h.counter() == metadata_from_handle(h).counter();
+            }
+
+            auto create()
+            {
+                auto idx(_next_idx);
+                ++_next_idx;
+
+                // Grow if needed.
+                if(idx > _metadata.size())
+                {
+                    _metadata.resize(_metadata.size() + 100);
+                }
+
+                // Handle points above. (TODO: generalize)
+                _metadata[idx]._index = idx;
+
+                handle_type h{idx, _metadata[idx]._counter};
+                VRM_CORE_ASSERT(valid_handle(h));
+
+                return h;
+            }
+
+            void destroy(const handle_type& h)
+            {
+                VRM_CORE_ASSERT(valid_handle(h));
+
+                // Get corresponding metadata and invalidate it.
+                auto& m(metadata_from_handle(h));
+                ++(m._counter);
+
+                // Swap indices and `pop_back` (TODO:)
+                auto& last_m(_metadata[_next_idx]);
+                std::swap(m._index, last_m._index);
+                --_next_idx;
+
+                VRM_CORE_ASSERT(!valid_handle(h));
+            }
+        };
+
         using index_type = sz_t;
         using counter_type = std::int8_t;
+
+        using test_hh = handle_helper<index_type, counter_type>;
 
         struct handle_data
         {
             index_type _index;
             counter_type _counter;
+
+            // TODO: meh
+            VRM_CORE_ALWAYS_INLINE handle_data() noexcept : _index{0},
+                                                            _counter{0}
+            {
+            }
 
             VRM_CORE_ALWAYS_INLINE handle_data(index_type index,
                 counter_type counter) noexcept : _index{index},
@@ -61,6 +207,7 @@ VRM_CORE_NAMESPACE
             using base_type = impl::base_delegate<TFunction, TSignature>;
             std::vector<handle_data> _handle_data;
 
+
             auto& handle_data_from_handle(const handle_type& h) noexcept
             {
                 VRM_CORE_ASSERT_OP(_handle_data.size(), >, h.target_idx());
@@ -81,13 +228,22 @@ VRM_CORE_NAMESPACE
 
             void erase_at(sz_t idx)
             {
-                VRM_CORE_ASSERT_OP(this->_functions.size(), >, idx);
-                this->_functions.erase(std::begin(this->_functions) + idx);
+                // VRM_CORE_ASSERT_OP(this->_functions.size(), >, idx);
+                // this->_functions.erase(std::begin(this->_functions) + idx);
 
-                for(auto i(idx); i < _handle_data.size(); ++i)
-                {
-                    _handle_data[i]._index -= 1;
-                }
+                auto to_erase_idx(idx);
+                auto last_idx(this->_functions.size() - 1);
+
+                auto& to_erase(this->_functions[to_erase_idx]);
+                auto& last(this->_functions[last_idx]);
+
+                auto& to_erase_handle(_handle_data[to_erase_idx]);
+                auto& last_handle(_handle_data[last_idx]);
+
+                std::swap(to_erase, last);
+                std::swap(to_erase_handle._index, last_handle._index);
+
+                this->_functions.pop_back();
             }
 
         public:
@@ -100,14 +256,9 @@ VRM_CORE_NAMESPACE
                 auto idx(this->_functions.size());
                 this->emplace_function(FWD(f));
 
-                if(_handle_data.size() <= idx)
-                {
-                    _handle_data.emplace_back(idx, 0);
-                }
-                else
-                {
-                    _handle_data[idx]._index = idx;
-                }
+                // TODO: meh
+                _handle_data.resize(this->_functions.size());
+                _handle_data[idx]._index = idx;
 
                 handle_type h{idx, _handle_data[idx]._counter};
                 VRM_CORE_ASSERT(valid_handle(h));
@@ -118,8 +269,9 @@ VRM_CORE_NAMESPACE
             void operator-=(const handle_type& h)
             {
                 VRM_CORE_ASSERT(valid_handle(h));
-                ++(handle_data_from_handle(h)._counter);
-                erase_at(h.target_idx());
+                auto& metadata(handle_data_from_handle(h));
+                ++(metadata._counter);
+                erase_at(metadata._index);
                 VRM_CORE_ASSERT(!valid_handle(h));
             }
         };
--- test/core/experimental/delegate/dynamic_delegate.cpp
+++ test/core/experimental/delegate/dynamic_delegate.cpp
@@ -85,10 +85,8 @@ void prev_tests()
     }
 }
 
-int main()
+void unsub_test0()
 {
-    prev_tests();
-
     dynamic_delegate<int(int)> d;
     auto h_doubler = d += [](int x)
     {
@@ -137,6 +135,213 @@ int main()
     TEST_ASSERT_OP(fold_res(3), ==, 9);
 
     (void)h_triple;
+}
+
+void unsub_test1()
+{
+    int f0 = 0;
+    int f1 = 0;
+    int f2 = 0;
+    int f3 = 0;
+
+    auto reset = [&]
+    {
+        f0 = f1 = f2 = f3 = 0;
+    };
+
+    auto test = [&](auto x0, auto x1, auto x2, auto x3)
+    {
+        std::cout << f0 << " " << f1 << " " << f2 << " " << f3 << "\n";
+
+        TEST_ASSERT_OP(f0, ==, x0);
+        TEST_ASSERT_OP(f1, ==, x1);
+        TEST_ASSERT_OP(f2, ==, x2);
+        TEST_ASSERT_OP(f3, ==, x3);
+
+        reset();
+    };
+
+    test(0, 0, 0, 0);
+
+    {
+        dynamic_delegate<void()> d;
+
+        auto s0 = d += [&]
+        {
+            f0 = 1;
+        };
+        auto s1 = d += [&]
+        {
+            f1 = 1;
+        };
+        auto s2 = d += [&]
+        {
+            f2 = 1;
+        };
+        auto s3 = d += [&]
+        {
+            f3 = 1;
+        };
+
+        d();
+        test(1, 1, 1, 1);
+
+        d -= s0;
+        d();
+        test(0, 1, 1, 1);
+
+        d -= s3;
+        d();
+        test(0, 1, 1, 0);
+
+        d -= s1;
+        d();
+        test(0, 0, 1, 0);
+
+        d -= s2;
+        d();
+        test(0, 0, 0, 0);
+    }
+
+    {
+        dynamic_delegate<void()> d;
+
+        auto s0 = d += [&]
+        {
+            f0 = 1;
+        };
+        auto s1 = d += [&]
+        {
+            f1 = 1;
+        };
+        auto s2 = d += [&]
+        {
+            f2 = 1;
+        };
+        auto s3 = d += [&]
+        {
+            f3 = 1;
+        };
+
+        d();
+        test(1, 1, 1, 1);
+
+        d -= s0;
+        d();
+        test(0, 1, 1, 1);
+
+        d -= s1;
+        d();
+        test(0, 0, 1, 1);
+
+        d -= s2;
+        d();
+        test(0, 0, 0, 1);
+
+        d -= s3;
+        d();
+        test(0, 0, 0, 0);
+    }
+
+    {
+        dynamic_delegate<void()> d;
+
+        auto s0 = d += [&]
+        {
+            f0 = 1;
+        };
+        auto s1 = d += [&]
+        {
+            f1 = 1;
+        };
+        auto s2 = d += [&]
+        {
+            f2 = 1;
+        };
+        auto s3 = d += [&]
+        {
+            f3 = 1;
+        };
+
+        d();
+        test(1, 1, 1, 1);
+
+        d -= s3;
+        d();
+        test(1, 1, 1, 0);
+
+        d -= s2;
+        d();
+        test(1, 1, 0, 0);
+
+        d -= s1;
+        d();
+        test(1, 0, 0, 0);
+
+        d -= s0;
+        d();
+        test(0, 0, 0, 0);
+    }
+}
+
+void unsub_test2()
+{
+    dynamic_delegate<int(int)> d;
+    auto h_adder = d += [](int x)
+    {
+        return x + 1;
+    };
+    auto h_doubler = d += [](int x)
+    {
+        return x * 2;
+    };
+
+    auto fold_res([&d](auto x)
+        {
+            auto v = d.call_and_return_vector(x);
+            int acc = 0;
+            for(auto dv : v) acc += dv;
+            return acc;
+        });
+
+    TEST_ASSERT_OP(fold_res(0), ==, 1);
+    TEST_ASSERT_OP(fold_res(1), ==, 4);
+    TEST_ASSERT_OP(fold_res(2), ==, 7);
+    TEST_ASSERT_OP(fold_res(3), ==, 10);
+
+    d -= h_adder;
+
+    TEST_ASSERT_OP(fold_res(0), ==, 0);
+    TEST_ASSERT_OP(fold_res(1), ==, 2);
+    TEST_ASSERT_OP(fold_res(2), ==, 4);
+    TEST_ASSERT_OP(fold_res(3), ==, 6);
+
+    d -= h_doubler;
+
+    TEST_ASSERT_OP(fold_res(0), ==, 0);
+    TEST_ASSERT_OP(fold_res(1), ==, 0);
+    TEST_ASSERT_OP(fold_res(2), ==, 0);
+    TEST_ASSERT_OP(fold_res(3), ==, 0);
+
+    auto h_triple = d += [](int x)
+    {
+        return x * 3;
+    };
+
+    TEST_ASSERT_OP(fold_res(0), ==, 0);
+    TEST_ASSERT_OP(fold_res(1), ==, 3);
+    TEST_ASSERT_OP(fold_res(2), ==, 6);
+    TEST_ASSERT_OP(fold_res(3), ==, 9);
+
+    (void)h_triple;
+}
+
+int main()
+{
+    prev_tests();
+    unsub_test0();
+    unsub_test1();
+    unsub_test2();
 
     return 0;
 }
